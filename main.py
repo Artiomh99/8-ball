fd6f638e099fbf407169713d665bf554dc889c77
Artiomh99 <holovko.artem@gmail.com>
2021-08-13 13:20:12 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+name = ""
+
+question = ""
+
+
+
+answer = ""
+import random
+random_number = random.randint(1, 10)
+
+
+if random_number == 1:
+  answer = "Yes - definitely."
+elif random_number == 2:
+  answer = "It is decidedly so."
+elif random_number == 3:
+  answer = "Without a doubt."
+elif random_number == 4:
+  answer = "reply haze, try again."
+elif random_number == 5:
+  answer = "Ask again later."
+elif random_number == 6:
+  answer = "Better not tell you now."
+elif random_number == 7:
+  answer = "My sources say no."
+elif random_number == 8:
+  answer = "Outlook not so good."
+elif random_number == 9:
+  answer = "Very doubtful."
+elif random_number == 10:
+  answer = "Probably... but not for sure."
+else:
+  answer = "Error"
+
+if name == "":
+  print("Question:" + question)
+else:
+  print(name + " asks: " + question)
+  print("Magic 8-Ball's answer: " + answer)
+
+if question == "":
+  print("enter a question for the Magic B-ball")
+else:
+  print(name + "asks:" + question)
+  print("Magic 8-Ball's answer: " + answer)
+
+
+
+
+
